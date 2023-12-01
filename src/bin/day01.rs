a683386f5b5bb604a3ee2d1bba040c05ddfa3c6c
glmdgrielson <zapperchamp1@gmail.com>
2023-12-01 11:11:33 -0500

    Solve part 2 of Day 01
    So this involves a bit of a silly hack.
    
    Problem - The code for finding the last digit of a line was not working.
    Solution - Rewrite the code so that it finds the first digit of a
    reversed copy of the string, using the code I know works.
    
    Also, 3 spelled backwards is "eerht" for future reference.

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -31,7 +31,11 @@ fn part_two(data: &str) -> usize {
     let mut sum = 0;
 
     for line in data.lines() {
-        let (one, two) = find_digit(line.to_string());
+        let one = find_digit(line);
+
+        let rev: String = line.chars().rev().collect();
+        let two = rfind_digit(&rev);
+        // println!("Line value: {}{}", one, two);
 
         let total = one * 10 + two;
         sum += total as usize;
@@ -40,7 +44,7 @@ fn part_two(data: &str) -> usize {
     return sum;
 }
 
-fn find_digit(line: String) -> (usize, usize) {
+fn find_digit(line: &str) -> usize {
     let numbers = [
         "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
     ];
@@ -53,15 +57,6 @@ fn find_digit(line: String) -> (usize, usize) {
             None => None,
         })
         .min_by_key(|(_, idx)| idx.clone());
-
-    // Find the number closest to the right.
-    let search_r = numbers
-        .iter()
-        .filter_map(|s| match line.rfind(s) {
-            Some(idx) => Some((s, idx)),
-            None => None,
-        })
-        .max_by_key(|(_, idx)| idx.clone());
 
     // let one = text_to_number(search_l);
     let one = if let Some((number, index)) = search_l {
@@ -91,41 +86,68 @@ fn find_digit(line: String) -> (usize, usize) {
             .map(|c| c.to_digit(10).unwrap())
             .expect("Digit not found") as usize
     };
+    one
+}
 
-    let two = if let Some((number, index)) = search_r {
-        let number = text_to_number(number);
+/// This is a terrible hack to made the code that WORKS
+/// replace the code that doesn't. Essentially, this is
+/// the same as `find_digit` except that it works on a
+/// reversed copy of the string, so that it can find the
+/// LAST instance of a digit.
+fn rfind_digit(line: &str) -> usize {
+    let numbers = [
+        "eno", "owt", "eerht", "ruof", "evif", "xis", "neves", "thgie", "enin",
+    ];
 
-        // We know every line has a digit in it, because
-        // part one requires it, so this mess is technically
-        // okay to do.
-        //
-        // Unfortunately, since we have to put in the call to
-        // `.rev` here, this index needs `nth_back` to find.
-        let digit_idx = line.chars().rev().position(|c| c.is_ascii_digit()).unwrap();
+    // Find the number closest to the left.
+    let search = numbers
+        .iter()
+        .filter_map(|s| match line.find(s) {
+            Some(idx) => Some((s, idx)),
+            None => None,
+        })
+        .min_by_key(|(_, idx)| idx.clone());
 
-        // Check if this is the latest number.
-        if digit_idx > index {
-            // The digit came later, so we need to turn the
-            // index into a digit.
+    // let one = text_to_number(search_l);
+    let res = if let Some((number, index)) = search {
+        let number = match number {
+            &"eno" => 1,
+            &"owt" => 2,
+            &"eerht" => 3,
+            &"ruof" => 4,
+            &"evif" => 5,
+            &"xis" => 6,
+            &"neves" => 7,
+            &"thgie" => 8,
+            &"enin" => 9,
+            _ => unreachable!("Errant backwards number detected")
+        };
 
-            // We know that this index exists because we literally just found it.
-            let ch = line.chars().nth_back(digit_idx).unwrap();
-
-            // We know that this is a digit due to the test we made
-            // to find this index.
-            ch.to_digit(10).unwrap() as usize
+        // We know every line has a digit in it, because part one
+        // requires it, so this is safe.
+        let digit_idx = line.chars().position(|c| c.is_ascii_digit()).unwrap();
+        if digit_idx < index {
+            // The digit came first, so we need to turn
+            // the index we have into a digit.
+            line.chars()
+                .nth(digit_idx)
+                .map(|c| c.to_digit(10))
+                // Verify the mapping worked.
+                .unwrap()
+                // Verify that we found the digit.
+                .unwrap() as usize
         } else {
             number
         }
     } else {
-        // This line has no text digit to find,
+        // This line _has_ no text digit to find
         // so we run the same check from part one.
         line.chars()
-            .rfind(|c| c.is_ascii_digit())
+            .find(|c| c.is_ascii_digit())
             .map(|c| c.to_digit(10).unwrap())
             .expect("Digit not found") as usize
     };
-    (one, two)
+    res
 }
 
 /// Turn a textual number into a digit.
@@ -149,8 +171,6 @@ fn main() {
 
     println!("Part one solution is: {}", part_one(&input));
 
-    assert_eq!(find_digit("eightwo1three".to_string()), (8, 3));
-
     println!("Part two solution is: {}", part_two(&input));
     // let mut lines = input.lines();
     // print!("{}", lines.next().expect("Missing data"));
@@ -163,7 +183,7 @@ mod test {
 
     #[test]
     fn example_two() {
-        let input = read_to_string("input/day01-test.txt").expect("File could not be read.");
+        let input = read_to_string("src/input/day01-test.txt").expect("File could not be read.");
 
         assert_eq!(part_two(&input), 281);
     }
