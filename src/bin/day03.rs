557c348714e73a72d90bc74a4ff83e74119d0eca
glmdgrielson <zapperchamp1@gmail.com>
2023-12-02 10:49:23 -0500

    Add Day 03 skeleton
    I'm afraid of what monster we're going to get for tomorrow's puzzle.

---
+++ src/bin/day03.rs
@@ -0,0 +1,22 @@
+//! Day 03 of Advent of Code 2023
+
+use std::fs::read_to_string;
+
+fn parse_input(input: &str) {
+    unimplemented!();
+}
+
+fn part_one() {}
+
+#[allow(dead_code)]
+fn part_two() {
+    unimplemented!("Part one incomplete!")
+}
+
+fn main() {
+    let input = read_to_string("src/input/day03.txt").expect("Could not read input");
+    let data = parse_input(&input);
+}
+
+#[cfg(test)]
+mod test {}
