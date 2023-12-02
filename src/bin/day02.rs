8a681d5ecd271e1eb483baecd39dcd3480a59a3b
glmdgrielson <zapperchamp1@gmail.com>
2023-12-02 08:57:13 -0500

    Attempt to solve part 1 of Day 02
    That lovely moment of "it works on the test data and not on the real
    thing" and I would love to know WHY.

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -7,7 +7,7 @@
 use std::fs::read_to_string;
 
 /// The number of cubes per game.
-#[derive(Clone, Default, Debug)]
+#[derive(Clone, Default, Debug, PartialEq)]
 struct Game {
     /// The game ID
     pub id: usize,
@@ -29,7 +29,7 @@ struct Game {
 /// It is not guaranteed that this will happen a fixed
 /// number of times or in a fixed order, nor will every
 /// color show up every round.
-fn parse_input(data: String) -> Vec<Game> {
+fn parse_input(data: &str) -> Vec<Game> {
     let mut vec = Vec::new();
 
     for line in data.lines() {
@@ -88,7 +88,38 @@ fn parse_input(data: String) -> Vec<Game> {
     vec
 }
 
-fn part_one() {}
+/// Part 1
+/// ------
+/// If we assume the bag the cubes are pulled from
+/// only contains 12 red cubes, 13 green cubes,
+/// and 14 blue cubes, how many of these games can
+/// actually be physically possible?
+///
+/// We are assuming for this part that the cubes are
+/// being put BACK into the bag, but that feels like
+/// the sort of assumption part two will tell us is
+/// false...
+fn part_one(data: &[Game]) -> usize {
+    let mut sum = 0;
+
+    for game in data {
+        if game.red_max > 12 {
+            continue;
+        }
+
+        if game.green_max > 13 {
+            continue;
+        }
+
+        if game.green_max > 14 {
+            continue;
+        }
+
+        sum += game.id;
+    }
+
+    sum
+}
 
 fn part_two() {
     unimplemented!("Part one incomplete!")
@@ -96,8 +127,89 @@ fn part_two() {
 
 fn main() {
     let input = read_to_string("input/day02.txt").expect("Could not read input data");
-    let games = parse_input(input);
+    let games = parse_input(&input);
+
+    println!("Sum of valid games is {}", part_one(&games));
 }
 
 #[cfg(test)]
-mod test {}
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_parse_input() {
+        let example = vec![
+            Game {
+                id: 1,
+                red_max: 4,
+                green_max: 2,
+                blue_max: 6,
+            },
+            Game {
+                id: 2,
+                red_max: 1,
+                green_max: 3,
+                blue_max: 4,
+            },
+            Game {
+                id: 3,
+                red_max: 20,
+                green_max: 13,
+                blue_max: 6,
+            },
+            Game {
+                id: 4,
+                red_max: 14,
+                green_max: 3,
+                blue_max: 15,
+            },
+            Game {
+                id: 5,
+                red_max: 6,
+                green_max: 3,
+                blue_max: 2,
+            },
+        ];
+        let data = read_to_string("src/input/day02-test.txt").expect("Could not read test data");
+
+        assert_eq!(example, parse_input(&data));
+    }
+
+    #[test]
+    fn test_part_one() {
+        let example = vec![
+            Game {
+                id: 1,
+                red_max: 4,
+                green_max: 2,
+                blue_max: 6,
+            },
+            Game {
+                id: 2,
+                red_max: 1,
+                green_max: 3,
+                blue_max: 4,
+            },
+            Game {
+                id: 3,
+                red_max: 20,
+                green_max: 13,
+                blue_max: 6,
+            },
+            Game {
+                id: 4,
+                red_max: 14,
+                green_max: 3,
+                blue_max: 15,
+            },
+            Game {
+                id: 5,
+                red_max: 6,
+                green_max: 3,
+                blue_max: 2,
+            },
+        ];
+
+        assert_eq!(part_one(&example), 8);
+    }
+}
