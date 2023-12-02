5245d0c15865e85d233a97f8c6207a039dccd422
glmdgrielson <zapperchamp1@gmail.com>
2023-12-01 20:51:00 -0500

    Prepare for Day 02
    This is all skeleton stuff at this point, but eh, I was bored and
    writing dummy code gave me something to do.

---
+++ src/bin/day02.rs
@@ -0,0 +1,16 @@
+//! Day 2 of Advent of Code
+
+use std::fs::read_to_string;
+
+fn part_one() {}
+
+fn part_two() {
+    unimplemented!("Part one incomplete!")
+}
+
+fn main() {
+    let input = read_to_string("input/day02.txt").expect("Could not read input data");
+}
+
+#[cfg(test)]
+mod test {}
