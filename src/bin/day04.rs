283a849ac6ac22d7eca3a108cea70b9b9eade72c
glmdgrielson <zapperchamp1@gmail.com>
2023-12-04 08:41:15 -0500

    Document and comment Day 04
    I already did a good amount of documentation during the solve, so this
    is more just a logic explaining pass this time around.

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -9,8 +9,15 @@
 use std::collections::{HashMap, HashSet};
 use std::fs::read_to_string;
 
+/// A ticket this elf was using.
 #[derive(Clone, Debug, PartialEq)]
 struct Ticket {
+    /// The number associated with this card.
+    ///
+    /// This is a `usize` because it's going to be used
+    /// as an index in part two, so it's convenient
+    /// to make the change here, even though Rust
+    /// defaults to making numbers `i`/`u32` most of the time.
     pub id: usize,
     /// The list of winning numbers for this ticket.
     pub expected: HashSet<u32>,
@@ -24,7 +31,7 @@ struct Ticket {
 /// win this ticket with.
 ///
 /// ```notrust
-/// Game N: A B C | W X Y Z
+/// Card N: A B C | W X Y Z
 /// ```
 fn parse_input(input: &str) -> Vec<Ticket> {
     input
@@ -36,12 +43,20 @@ fn parse_input(input: &str) -> Vec<Ticket> {
                 .expect("Invalid ticket header")
                 .trim_start();
 
+            // Remove the card number from the string and store it.
             let (id, ticket) = line.split_once(": ").expect("Missing ticket data");
+            // Convert the card number into an actual number.
             let id = id.parse().expect("Invalid ticket ID");
 
+            // Split the winning numbers from the guessed numbers.
             let (expected, actual) = ticket.split_once(" | ").expect("Invalid ticket format");
 
+            // Convert the various numbers into integers.
             let expected = expected
+                // We have to use this function because there
+                // isn't a constant separation between two
+                // consecutive numbers in this puzzle,
+                // which is a little annoying.
                 .split_whitespace()
                 .map(|n| n.parse::<u32>().unwrap())
                 .collect();
@@ -67,6 +82,7 @@ fn parse_input(input: &str) -> Vec<Ticket> {
 /// - For every number after the first on that ticket, double the score.
 fn part_one(data: &[Ticket]) -> u32 {
     data.iter().fold(0, |acc, ticket| {
+        // Find the numbers that won.
         let winners = ticket
             .expected
             .intersection(&ticket.actual)
@@ -76,6 +92,8 @@ fn part_one(data: &[Ticket]) -> u32 {
 
         let total = winners.len() as u32;
 
+        // The explicit tag on the two is necessary to get the compiler
+        // to shut up for some reason. I would love to know why.
         let score = if total >= 1 { 2_u32.pow(total - 1) } else { 0 };
 
         // println!("Card {}'s score is {}", ticket.id, score);
@@ -98,6 +116,12 @@ fn part_two(data: &[Ticket]) -> u32 {
     let mut ticket_count: HashMap<usize, u32> = data.iter().map(|t| (t.id, 1)).collect();
 
     data.iter().for_each(|ticket| {
+        // Get a separate handle on the number of copies
+        // of this ticket that we already have. We need
+        // the `clone` in here to satisfy the borrow checker
+        // since modifying a map that we're in the process
+        // of using is only questionably safe, which isn't
+        // good enough for the dang compiler.
         let this_count = ticket_count.get(&ticket.id).unwrap().clone();
         let winners = ticket
             .expected
@@ -116,7 +140,12 @@ fn part_two(data: &[Ticket]) -> u32 {
             // due to the constraints of the puzzle, but
             // Rust means I don't necessarily need to know that.
             ticket_count.entry(t).and_modify(|count| {
-                // Add another one to it.
+                // Add more tickets to it.
+                //
+                // We specifically need to add one ticket
+                // for every copy of this particular
+                // ticket that we have. This was something
+                // I had to figure out the hard way.
                 *count += this_count;
             });
         });
