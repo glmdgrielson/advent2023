e771609a8b4c2e06a9b67e510532da74fe65945a
glmdgrielson <zapperchamp1@gmail.com>
2023-12-07 08:18:44 -0500

    Factor out ParseError enum
    Now I can use this in future puzzles.

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+//! Common utilities for Advent of Code
+
+use thiserror::Error;
+use std::num::ParseIntError;
+
+#[derive(Clone, PartialEq, Debug, Error)]
+/// The common error type for parsing.
+pub enum ParseError {
+    #[error("Failed to find {0} in input")]
+    /// Parsing has failed to find
+    /// a required part of the input format.
+    InvalidFormat(&'static str),
+    #[error("Failed to parse number")]
+    /// A call to `String::parse` has failed.
+    ExpectedNumber(#[from] ParseIntError),
+}
