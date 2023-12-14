1c8d3c9e72190e30260ec0bdd783b7f071653483
glmdgrielson <zapperchamp1@gmail.com>
2023-12-14 15:01:18 -0500

    Solve part 1 of Day 14
    I get the feeling that this is gonna be an annoying one that needs a
    tricky optimization for part 2.

---
+++ src/bin/day14.rs
@@ -0,0 +1,146 @@
+//! Day 14 of Advent of Code
+//!
+//! Parabolic Reflector Dish
+//! ========================
+//!
+//! There's a, well, parabolic
+//! reflector dish that's a bit
+//! out of shape. We need to fix
+//! it before things can get
+//! back to normal.
+
+use std::fs::read_to_string;
+
+use simple_grid::Grid;
+
+use advent_2023::{ParseError, ParseResult};
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+enum Rock {
+    Ground,
+    Round,
+    Cube,
+}
+
+/// Input consists of a grid of
+/// rocks and ground. Ground is
+/// signified by a `.` character,
+/// while rocks can be either `#`
+/// or `O` depending on whether it
+/// is cubed or rounded.
+fn parse_input(input: &str) -> ParseResult<Grid<Rock>> {
+    let mut grid = Grid::new(0, 0, Vec::new());
+
+    for line in input.lines() {
+        let rocks = line
+            .chars()
+            .map(|ch| match ch {
+                '.' => Ok(Rock::Ground),
+                '#' => Ok(Rock::Cube),
+                'O' => Ok(Rock::Round),
+                _ => Err(ParseError::InvalidFormat("proper rock character")),
+            })
+            .collect::<ParseResult<Vec<Rock>>>()?;
+
+        grid.push_row(rocks);
+    }
+
+    Ok(grid)
+}
+
+fn tilt_grid(grid: &Grid<Rock>) -> Grid<Rock> {
+    let mut out = Grid::new(0, 0, vec![]);
+
+    for col in grid.columns() {
+        let column = grid.column_iter(col).map(|&rock| rock).collect::<Vec<_>>();
+
+        let chunks = column
+            .split(|&rock| rock == Rock::Cube)
+            .map(|chunk| {
+                let (rounds, grounds): (Vec<Rock>, Vec<Rock>) =
+                    chunk.iter().partition(|&rock| *rock == Rock::Round);
+                [rounds, grounds].concat()
+            })
+            .collect::<Vec<Vec<_>>>();
+
+        let column = chunks.join(&Rock::Cube);
+
+        out.push_column(column);
+    }
+
+    assert_eq!(grid.dimensions(), out.dimensions());
+    out
+}
+
+/// Part 1
+/// ------
+///
+/// Tilt the plane so that all of the
+/// rocks move upward. How much stress
+/// does this action place upon the dish?
+fn part_one(data: &Grid<Rock>) -> usize {
+    let grid = tilt_grid(data);
+
+    grid.rows()
+        .map(|row| {
+            let count = grid
+                .row_iter(row)
+                .filter(|&rock| *rock == Rock::Round)
+                .count();
+            count * (grid.height() - row)
+        })
+        .sum()
+}
+
+#[allow(unused)]
+fn part_two(data: &Grid<Rock>) {
+    unimplemented!();
+}
+
+fn main() {
+    let input = read_to_string("src/input/day14.txt").expect("Could not read input");
+    let data = parse_input(&input).expect("Parsing failed");
+
+    println!("The total load on the supports is {}", part_one(&data));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_parse_input() {
+        let input = read_to_string("src/input/day14-test.txt").expect("Could not read example");
+        let data = parse_input(&input).expect("Parsing failed");
+
+        let row = data.row_iter(0).collect::<Vec<_>>();
+        assert_eq!(row[0], &Rock::Round);
+        assert_eq!(row[5], &Rock::Cube);
+    }
+
+    #[test]
+    fn test_tilt_grid() {
+        let input = read_to_string("src/input/day14-test.txt").expect("Could not read example");
+        let data = parse_input(&input).expect("Parsing failed");
+
+        let grid = tilt_grid(&data);
+
+        let actual = grid.column_iter(0).map(|&rock| rock).collect::<Vec<_>>();
+        let expected: Vec<_> = [
+            vec![Rock::Round; 4],
+            vec![Rock::Ground; 4],
+            vec![Rock::Cube; 2],
+        ]
+        .concat();
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_part_one() {
+        let input = read_to_string("src/input/day14-test.txt").expect("Could not read example");
+        let data = parse_input(&input).expect("Parsing failed");
+
+        assert_eq!(part_one(&data), 136);
+    }
+}
